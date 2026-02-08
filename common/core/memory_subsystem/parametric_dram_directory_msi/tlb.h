a189a5feb32b93632c8cf0c98764da58ca847f1a
otegby <oskar.tegby@proton.me>
2026-02-08 16:12:59 +0100

    [Readability] Minor stylistic cleanups and readability improvements

--- common/core/memory_subsystem/parametric_dram_directory_msi/tlb.h
+++ common/core/memory_subsystem/parametric_dram_directory_msi/tlb.h
@@ -25,17 +25,17 @@ namespace ParametricDramDirectoryMSI
          UInt64 m_access, m_miss, m_alloc, m_bypass;
 	 UInt32 m_conf_counter = 2;
       public:
-         std::map<IntPtr, std::map<IntPtr, uint64_t> > hitCounter;
-	 std::map<IntPtr, uint64_t> curHit;
+         std::map<IntPtr, std::map<IntPtr, uint64_t>> hitCounter;
+         std::map<IntPtr, uint64_t> curHit;
          TLB(String name, String cfgname, core_id_t core_id, UInt32 num_entries, UInt32 associativity, TLB *next_level, UInt32 conf_count = 2);
          bool lookup(IntPtr address, SubsecondTime now, bool isIfetch, MemoryManager *mptr, bool allocate_on_miss = true);
          void allocate(IntPtr address, SubsecondTime now);
-	 void setDeadBit (IntPtr address);
+         void setDeadBit(IntPtr address);
          UInt32 give_size();
          void setL3Controller(CacheCntlr*);
-         void shadow_table_insert (IntPtr vpn);
-         bool shadow_table_search (IntPtr vpn);
-         IntPtr findHash (IntPtr ev_vpn, uint64_t bits);
+         void shadow_table_insert(IntPtr vpn);
+         bool shadow_table_search(IntPtr vpn);
+         IntPtr findHash(IntPtr ev_vpn, uint64_t bits);
    };
 }
 
