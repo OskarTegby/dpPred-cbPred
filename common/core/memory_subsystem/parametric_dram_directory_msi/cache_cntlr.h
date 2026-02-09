976a61bf3e9c8fd76f4d2d65c07352b2b5ac504b
otegby <oskar.tegby@proton.me>
2026-02-10 00:15:39 +0100

    [Refactor] Split the miss part of the if-else into its own function

--- common/core/memory_subsystem/parametric_dram_directory_msi/cache_cntlr.h
+++ common/core/memory_subsystem/parametric_dram_directory_msi/cache_cntlr.h
@@ -413,6 +413,7 @@ namespace ParametricDramDirectoryMSI
          void updateLLCSw(uint64_t latestTag, uint64_t pivotIndex, uint64_t set);
          int findTagInSet(uint64_t set, uint64_t tag);
          void handleLLCHit(uint64_t tag, int pivotIndex, uint64_t set);
+         void handleLLCMiss(uint64_t tag, uint64_t set);
          void accessLLCSw(IntPtr address);
 
          uint64_t getTagSwdef(IntPtr address);
