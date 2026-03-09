58f27ea963bb465555db12e02251069e56cef37b
Oskar Tegby <oskar.tegby@proton.me>
2026-03-09 02:58:13 +0100

    [Fix] Added fix to run the simulations with run.cfg

--- run_sn.sh
+++ run_sn.sh
@@ -63,7 +63,7 @@ shadow_table_size=$SHADOW_TABLE_SIZE
 INFO
 
     echo "  Running \$benchmark..."
-    $SNIPER -p "\$benchmark" -n $CORES -d "\$output_dir" \
+    $SNIPER -c run.cfg -p "\$benchmark" -n $CORES -d "\$output_dir" \
             > "\${output_dir}/stdout.log" 2> "\${output_dir}/stderr.log" &
 done
 
